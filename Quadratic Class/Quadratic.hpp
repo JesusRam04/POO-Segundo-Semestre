213d8c4d2fc0de8f90f46e8b1194bb2e89a604d4
Jesus Ramirez <A00835680@tec.mx>
2023-06-05 23:15:20 -0600

    changes

---
+++ Quadratic Class/Quadratic.hpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+
+class Quadratic {
+public:
+    Quadratic();
+    Quadratic(int, int, int);
+    // Setters
+    void setICoefA(int a) { iCoefA = a; }
+    void setICoefB(int b) { iCoefB = b; }
+    void setICoefC(int c) { iCoefC = c; }
+    // Getters
+    int getICoefA() { return iCoefA; }
+    int getICoefB() { return iCoefB; }
+    int getICoefC() { return iCoefC; }
+    void show();
+    //operators
+    Quadratic operator+(Quadratic ec2);
+    Quadratic operator-(Quadratic ec2);
+    Quadratic operator*(int iValue);
+    void operator+=(Quadratic ec2);
+    bool operator==(Quadratic ec2);
+    void operator++();
+private:
+    int iCoefA, iCoefB, iCoefC;
+};
+
+
+Quadratic::Quadratic() {
+    iCoefA = 1;
+    iCoefB = 1;
+    iCoefC = 1;
+}
+
+Quadratic::Quadratic(int iA, int iB, int iC) {
+    iCoefA = iA;
+    iCoefB = iB;
+    iCoefC = iC;
+}
+void Quadratic::show() {
+    if (iCoefA != 0) {
+        cout << iCoefA;
+        cout << "x^2 ";
+    }
+    if (iCoefB != 0) {
+        if (iCoefB > 0) cout << " + ";
+            cout << iCoefB;
+            cout << "x ";
+        }
+    if (iCoefC != 0) {
+        if (iCoefC > 0) cout << " + ";
+        cout << iCoefC << endl;
+    }
+}
+
+Quadratic Quadratic::operator+(Quadratic ec2) {
+    Quadratic ecRes;
+    ecRes.iCoefA = iCoefA + ec2.iCoefA;
+    ecRes.iCoefB = iCoefB + ec2.iCoefB;
+    ecRes.iCoefC = iCoefC + ec2.iCoefC;
+    return ecRes;
+}
+
+Quadratic Quadratic::operator-(Quadratic ec2) {
+    Quadratic ecRes;
+    ecRes.iCoefA = iCoefA - ec2.iCoefA;
+    ecRes.iCoefB = iCoefB - ec2.iCoefB;
+    ecRes.iCoefC = iCoefC - ec2.iCoefC;
+    return ecRes;
+}
+
+Quadratic Quadratic::operator*(int iValue) {
+    Quadratic ecRes;
+    ecRes.iCoefA = iCoefA * iValue;
+    ecRes.iCoefB = iCoefB * iValue;
+    ecRes.iCoefC = iCoefC * iValue;
+    return ecRes;
+}
+
+void Quadratic::operator+=(Quadratic ec2) {
+    iCoefA = iCoefA + ec2.iCoefA;
+    iCoefB = iCoefB + ec2.iCoefB;
+    iCoefC = iCoefC + ec2.iCoefC;
+}
+
+bool Quadratic::operator==(Quadratic ec2) {
+    if (iCoefA == ec2.iCoefA && iCoefB == ec2.iCoefB && iCoefC == ec2.iCoefC) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void Quadratic::operator++() {
+    iCoefC++;
+}
---
+++ Quadratic Class/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+#include "Quadratic.hpp"
+
+
+int main()
+{
+    int iA, iB, iC;
+    Quadratic ec1(5,3,2), ecRes;
+    Quadratic ec3 = ec1; // equation 1 y equation 3 have the same values.
+    cout << "Type in the coefficients for the second equation" << endl;
+    cin >> iA >> iB >> iC;
+    Quadratic ec2(iA, iB, iC);
+    /*********************************/
+    cout << endl;
+    cout << "=== Sum of Equations 1 and 2" << endl;
+    cout << "Equation 1 : " ;
+    ec1.show();
+    cout << "Equation 2 : ";
+    ec2.show();
+    ecRes = ec1 + ec2;
+    cout << "Result : ";
+    ecRes.show();
+    cout << "Equation 1 : " ;
+    ec1.show();
+    cout << "Equation 2 : ";
+    ec2.show();
+    cout << endl;
+    /*********************************/
+    cout << endl;
+    cout << "=== Subtraction of Equations 1 and 2" << endl;
+    cout << "Equation 1 : " ;
+    ec1.show();
+    cout << "Equation 2 : " ;
+    ec2.show();
+    ecRes = ec1 - ec2;
+    cout << "Result : ";
+    ecRes.show();
+    cout << "Equation 1 : " ;
+    ec1.show();
+    cout << "Equation 2 : ";
+    ec2.show();
+    cout << endl;
+    /*********************************/
+    cout << endl;
+    cout << "=== Multiply Equation 1 by the integer value 2" << endl;
+    cout << "Equation 1 : " ;
+    ec1.show();
+    ecRes = ec1 * 2;
+    cout << "Result : ";
+    ecRes.show();
+    cout << "Equation 1 : " ;
+    ec1.show();
+    cout << endl;
+    /*********************************/
+    cout << endl;
+    cout << "=== Compare equation 1 and equation 2 with ==, they must be different" << endl;
+    cout << "Equation 1 : " ;
+    ec1.show();
+    cout << "Equation 2 : " ;
+    ec2.show();
+    if (ec1 == ec2)
+    cout << "They are equal" << endl;
+    else
+    cout << "They are different " << endl;
+    /*********************************/
+    cout << endl;
+    cout << "=== Compare equation 1 and equation 3 with ==, they must be equal" << endl;
+    cout << "Equation 1 : " ;
+    ec1.show();
+    cout << "Equation 3 : " ;
+    ec3.show();
+    if (ec1 == ec3)
+    cout << "They are equal" << endl;
+    else
+    cout << "They are different" << endl;
+    /*********************************/
+    cout << endl;
+    cout << "=== Apply += to add Equation 2 to Equation 1" << endl;
+    cout << "Equation 1 : " ;
+    ec1.show();
+    cout << "Equation 2 : ";
+    ec2.show();
+    ec1 += ec2;
+    cout << "Equation 1 : " ;
+    ec1.show();
+    cout << "Equation 2 : ";
+    ec2.show();
+    cout << endl;
+    /*********************************/
+    cout << endl;
+    cout << "=== Apply ++ to Equation 1 " << endl;
+    cout << "Equation 1 : " ;
+    ec1.show();
+    ecRes = ec1;
+    ++ecRes;
+    cout << "Result : ";
+    ecRes.show();
+    cout << "Equation 1 : " ;
+    ec1.show();
+    return 0;
+}
